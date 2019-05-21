19be33e573e35dd68dcdad3a4a463e9a650be030
Jhosimar Agucia Fisco <jhosimar.aguaciafisco@aalto.fi>
2019-05-21 14:59:11 +0300

    the layer selection is done through ajax requests,TODO performance should be improved. The distance calculation is using the shifkey TODO we should discuss if we want to use shiftkey or maybe just rightclick

--- mapapp/urls.py
+++ mapapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ui, name="mapview"),
+    path('TIK_data/<int:floor>/', views.TIK, name="TIK"),
+    path('TUAS_data/<int:floor>/', views.TUAS, name="TUAS"),
 ]
--- mapapp/views.py
+++ mapapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import FloorLine
 from django.core.serializers import serialize
+from django.http  import HttpResponse
 
 # Create your views here.
 
@@ -8,10 +9,28 @@ def ui(request):
     selectedfloor = '1st'
     if request.method == "POST":
         selectedfloor = request.POST['visibfloors']
-    fpick = FloorLine.objects.filter(level=int(selectedfloor[0]))
+    fpick = FloorLine.objects.filter(level=int(selectedfloor[0]),building_name='TUAS_TALO')
     floor = serialize('geojson', fpick)
     context = {'floor' : floor, 'selectedfloor' : selectedfloor}
     return render(request, 'mapview.html', context)
 
+def TIK(request, floor):
+    selectedfloor = floor
+    if request.method == "POST":
+        selectedfloor = request.POST['visibfloors']
+    fpick = FloorLine.objects.filter(level=int(selectedfloor),building_name='TIK_TALO')
+    TIK_floor = serialize('geojson', fpick)
+    context = {'floor' : TIK_floor, 'selectedfloor' : selectedfloor}
+    return HttpResponse(TIK_floor,content_type='json')
+
+def TUAS(request, floor):
+    selectedfloor = floor
+    if request.method == "POST":
+        selectedfloor = request.POST['visibfloors']
+    fpick = FloorLine.objects.filter(level=int(selectedfloor),building_name='TUAS_TALO')
+    TUAS_floor = serialize('geojson', fpick)
+    context = {'floor' : TUAS_floor, 'selectedfloor' : selectedfloor}
+    return HttpResponse(TUAS_floor,content_type='json')
+   
 
 
