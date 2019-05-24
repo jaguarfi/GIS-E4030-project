717068023882276777208aca63f022af1a2a1242
Jhosimar Agucia Fisco <jhosimar.aguaciafisco@aalto.fi>
2019-05-24 20:13:44 +0300

    new heroku build package

--- mapservice/settings.py
+++ mapservice/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku 
+from os import environ
+
+GEOS_LIBRARY_PATH = environ.get('GEOS_LIBRARY_PATH')
+GDAL_LIBRARY_PATH = environ.get('GDAL_LIBRARY_PATH')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
